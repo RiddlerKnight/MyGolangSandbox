ac3b72dd1c486fb6dbb027920b917a10a9b1c0f6
Bawornsit.C <puksigon@gmail.com>
2021-07-24 15:16:15 +0700

    add more comprehensive output

--- typeconvert/type_convert.go
+++ typeconvert/type_convert.go
@@ -4,33 +4,46 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-var foo int
+var foo int64
 var bar string
 var baz bool
 
 func main() {
 	fmt.Println("Type Convert Start")
+	fmt.Println("======== print int directly =======")
 	foo = 10
 	fmt.Println(foo)
 
-	fmt.Println("===============")
+	fmt.Println("======== convert int to string =======")
 	newfoo := string(foo) // this will not work
 	fmt.Println(newfoo)
 
-	fmt.Println("===============")
+	fmt.Println("======= use fmt ========")
 	newfoo2 := fmt.Sprint(foo)
 	fmt.Println(newfoo2)
 
+	fmt.Println("======== use strconv =======")
+	newfoo3 := strconv.FormatInt(foo, 10)
+	fmt.Println(newfoo3)
+
+	fmt.Println("======== convert string to int =======")
 	bar = "20"
 	//barnew = int(bar)  // this will not work and got error
 	//fmt.Println(barnew)
 
-	newbar, newbarerror := fmt.Print(bar)
-	fmt.Printf("%d", newbar)
+	newbar, newbarerror := strconv.ParseInt(bar, 10, 64)
+	fmt.Println(newbar)
 
 	if newbarerror != nil {
 		//error here
+		panic(newbarerror)
 	}
+
+	fmt.Println("======== convert bool to string =======")
+	//newbaz := string(baz) // not work
+	newbaz := strconv.FormatBool(baz)
+	fmt.Println(newbaz)
 }
