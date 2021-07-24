f41439754e069bdbada19d368d1e9b463eb199cd
Bawornsit.C <puksigon@gmail.com>
2021-07-24 23:50:08 +0700

    Add Interface_ex

--- interface_ex/interface_ex.go
+++ interface_ex/interface_ex.go
@@ -1,10 +1,29 @@
 package main
 
-type IFoo interface {
-	Read()
-	Write()
+import "fmt"
+
+type IHumanCap interface {
+	Read(value string)
+	Write(value string)
+}
+
+func (s Person) Read(value string) {
+	fmt.Println("Read value: ", value)
+	fmt.Println("Who Read : ", s.name)
+}
+
+func (s Person) Write(value string) {
+	fmt.Println("Write value: ", value)
+	fmt.Println("Who Write : ", s.name)
+}
+
+type Person struct {
+	name string
 }
 
 func main() {
+	p := Person{name: "Yuna"}
+	p.Read("Read From main")
 
+	p.Write("Write From main")
 }
